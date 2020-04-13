8aeb20cc4ed72e60d1ba1f909bd3a467b4abceb9
David Mai <maid2@uw.edu>
2020-04-13 12:39:56 -0700

    completed 6-4

--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -1,15 +1,28 @@
 # Exercise 4: functions and conditionals
 
+library(lintr)
+
 # Define a function `is_twice_as_long` that takes in two character strings, and 
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(str1, str2) {
+  str1_length <- nchar(str1)
+  str2_length <- nchar(str2)
+  str_length_diff <- abs(str1_length - str2_length)
+  if (str1_length > str2_length) {
+    return (str_length_diff >= str2_length)  
+  } else {
+    return (str_length_diff >= str1_length)
+  }
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
+is_twice_as_long("hope", "cool")
+is_twice_as_long("hope", "hopefully")
+is_twice_as_long("informatic", "david")
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -18,7 +31,23 @@
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
 
+describe_difference <- function(str1, str2) {
+  if (nchar(str1) > nchar(str2)) {
+    str_length_diff <- nchar(str1) - nchar(str2)
+    return(paste("Your first string is longer by", str_length_diff, "characters"))
+    
+  } else if (nchar(str2) > nchar(str1)) {
+    str_length_diff <- nchar(str2) - nchar(str1)
+    return(paste("Your second string is longer by", str_length_diff, "characters"))
+    
+  } else {
+    return(paste("Your strings are the same length!"))
+  }
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
 
+describe_difference("shoe", "hot")
+describe_difference("cool", "frozen")
+describe_difference("cot", "koi")
